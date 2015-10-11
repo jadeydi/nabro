b25a0a95f760d8ede771028b02c1535863592fa0
jadeydi <im.yuqlee@gmail.com>
2015-10-11 16:40:06 +0800

    Add js validation

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,2 +1,20 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+$(function() {
+  var date = moment().utc().startOf('day') / 1000,
+      lastClickedAt = $('.js-dull-btn').data('last-clicked-at');
+  $(".js-dull-btn").on("click", function(event) {
+    if ($(this).data("last-clicked-at") == "") {
+      event.preventDefault();
+      event.stopPropagation();
+
+      $('.js-need-sign-in').show();
+    } else if (date <= lastClickedAt) {
+      event.preventDefault();
+      event.stopPropagation();
+
+      $('.js-already-clicked').show();
+    }
+  });
+});
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def last_clicked_at
+    current_user.blank? ? "" : current_user.last_clicked_at.beginning_of_day.to_i
+  end
 end
