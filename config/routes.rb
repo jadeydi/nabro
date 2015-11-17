310ac6d932a7f18fcef52ab1022943beb3107039
jadeydi <im.yuqlee@gmail.com>
2015-11-17 22:12:18 +0800

    Add missing translation

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,5 @@
 Rails.application.routes.draw do
 
-  scope module: :api do
-#    constraints subdomain: 'api' do
-    resources :base, only: [:index]
-    resource :users, only: [] do
-      collection do
-        post :sign_up
-        post :sign_in
-      end
-    end
-
-    resource :states, only: [:create]
-#    end
-  end
-
   root 'home#index'
 
   devise_for :users, controllers: { omniauth_callbacks: "sessions" }
@@ -26,5 +12,19 @@ Rails.application.routes.draw do
   end
 
   resources :states, only: [:create]
+
+  scope module: :api do
+    constraints subdomain: 'api' do
+    resources :base, only: [:index]
+    resource :users, only: [] do
+      collection do
+        post :sign_up
+        post :sign_in
+      end
+    end
+
+    resource :states, only: [:create]
+    end
+  end
 
 end
