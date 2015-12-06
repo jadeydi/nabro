f96626a57d1496b4f0cf9add4a838fbfc4687079
jadeydi <im.yuqlee@gmail.com>
2015-12-06 17:59:13 +0800

    Fix qiniu config bug

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,8 +2,8 @@
   config.storage             = :qiniu
   config.qiniu_access_key    = $system.qiniu.access_key
   config.qiniu_secret_key    = $system.qiniu.secret_key
-  config.qiniu_bucket        = $system.bucket
-  config.qiniu_bucket_domain = $system.domain
+  config.qiniu_bucket        = $system.qiniu.bucket
+  config.qiniu_bucket_domain = $system.qiniu.domain
   config.qiniu_block_size    = 4*1024*1024
   config.qiniu_protocol      = "http"
   config.qiniu_can_overwrite = true
