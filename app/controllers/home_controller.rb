5fa2539adb7275eaaebbac67948efb3ceb540b3b
jadeydi <im.yuqlee@gmail.com>
2016-04-12 19:44:19 +0800

    Add background image to home

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,5 @@
 class HomeController < ApplicationController
+  layout 'visitor'
   def index
-    if current_user && Time.now.utc.to_date <= current_user.last_clicked_at.to_date
-      @try = current_user.last_try
-    else
-      @try = nil
-    end
   end
 end
