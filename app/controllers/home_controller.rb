6ecd028a14564a49cbc4864b9e13f9d0c1ab645f
jadeydi <im.yuqlee@gmail.com>
2016-04-20 23:44:13 +0800

    Update homepage style

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   layout 'visitor'
   def index
-    @task = current_user.tasks.where(status: 0).last if current_user
+    @count = Task.count
   end
 end
